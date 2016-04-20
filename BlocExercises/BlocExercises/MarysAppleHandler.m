796b027143c653370ba81e9d458ec2873f0e9811
Paul Burville <paul@imls.com>
2016-04-19 23:59:44 -0400

    Checkpoint 11 completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,10 +12,28 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
-
-    /* WORK HERE */
     
-    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
+    /* WORK HERE */
+    if (dollars>4) {
+        if (dollars==5)
+                { itemToReturn = @"have some gum" ;
+                    }
+            else if (dollars==6) {
+                itemToReturn = @"have an apple";
+                    }
+            else if (dollars==1000) {
+                itemToReturn = @"have an Apple computer";
+                    }
+            else if (dollars==1000000000) {
+                itemToReturn = @"have The Big Apple";
+                    }
+// message = ((dollars= 5) ? @"have some gum" : @"have an apple");
+        //    itemToReturn = [NSString stringWithFormat:@"For $%ld, Mary can: %@", dollars, message];
+        
+            }
+    else {
+        itemToReturn = @"get out of my store";
+        }
     return itemToReturn;
 }
 
