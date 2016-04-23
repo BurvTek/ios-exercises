dc17ccad49d6c4267c6bed8902b99a3776770d0d
Paul Burville <paul@imls.com>
2016-04-23 01:49:26 -0400

    Checkpoint 13 for Arrays completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,48 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    NSArray *starTrekCharactersArray = [characterString componentsSeparatedByString:@";"];
+    [starTrekCharactersArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+     NSLog(@"Character #%ld: %@", idx + 1, obj);
+    }];
+    
+    return starTrekCharactersArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    NSString *starTrekiesString = [characterArray componentsJoinedByString:@";"];
+    NSLog(@"Who do we have? %@", starTrekiesString);
+    
+    return starTrekiesString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *myMutableTrekies = [NSMutableArray arrayWithCapacity:5];
+    for (NSInteger idx = 0; idx < characterArray.count; idx++) {
+        [myMutableTrekies addObject:characterArray[idx]];
+    }
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [myMutableTrekies sortUsingDescriptors:@[sortDescriptor]];
+
+    return myMutableTrekies;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
+    BOOL worfIsHere = NO;
+    NSMutableArray *myMutableTrekies = [NSMutableArray arrayWithCapacity:5];
+    for (NSInteger idx = 0; idx < characterArray.count; idx++) {
+        [myMutableTrekies addObject:characterArray[idx]];
+    }
+    NSPredicate *containsThe = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [myMutableTrekies filterUsingPredicate:containsThe];
+    for (NSString *worfName in myMutableTrekies) {
+        worfIsHere = YES;
+    }
+    return worfIsHere;
 }
 
 @end
